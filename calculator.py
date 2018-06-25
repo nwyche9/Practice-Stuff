e907a68d678155391e51262948a26ef90692d4a9
Nate <nwyche9@gmail.com>
2018-06-24 21:50:15 -0400

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -6,8 +6,8 @@ Created on Sun Jun 24 15:24:43 2018
 """
 
 def calc(a, b, op):
-    if op not in '+-/*':
-        return 'Please only use one of these characters: "+,-,/,*"'
+    if op not in '+-/*^':
+        return 'Please only use one of these characters: "+,-,/,*,^"!'
     
     if op =='+':
         return(str(a) + ' '+ op+ ' '+str(b) + ' = ' + str(a + b))
@@ -17,13 +17,15 @@ def calc(a, b, op):
         return(str(a)+' '+op+' '+str(b)+' = '+str(a/b))
     if op == '*':
         return(str(a)+' '+op+' '+str(b)+' = '+str(a*b))
+    if op == '^':
+        return(str(a)+' '+op+' '+str(b)+' = '+str(a**b))
         
 
 def main():
     
     a = int(input('What is your first number??:  '))
     b=int(input('What is your second number??:  '))
-    op = input('What operation would you like to preform??\n Choices are "+,-,/,*":  ')
+    op = input('What operation would you like to preform??\n Choices are "+,-,/,*,^":  ')
     
     
     print(calc(a,b,op))
